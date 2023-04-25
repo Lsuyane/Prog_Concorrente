07428d086f9e7f39fd08c5b75d8080ec7b680b55
Lsuyane <92032032+Lsuyane@users.noreply.github.com>
2023-04-25 08:12:28 -0300

    Update ATIVIDADE6.c

--- ATIVIDADE6.c
+++ ATIVIDADE6.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#define TAMANHO 1000000
+
+int main(){
+  
+    int i;
+    double start_time, end_time;
+    double *vetor;
+    double maior = 0.0;
+    vetor = (double *) malloc(sizeof(double) * TAMANHO);
+
+    #pragma omp parallel for
+    for(i = 0; i < TAMANHO; i++) {
+        vetor[i] = (double) rand() / RAND_MAX;
+    }
+    start_time = omp_get_wtime();
+
+    #pragma omp parallel for reduction(max:maior)
+    for(i = 0; i < TAAMANHO; i++) {
+        if(vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+
+    end_time = omp_get_wtime();
+
+    printf("Maior valor do vetor: %f\n", maior);
+    printf("Tempo de execução: %f \n", end_time - start_time);
+
+    free(vetor);
+
+    return 0;
+}
 
